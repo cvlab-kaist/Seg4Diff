b944e74c2844aac0dc90c2f6826f69a74a1911f1
kchyun <judy8823@korea.ac.kr>
2025-09-18 21:56:49 +0900

    init project page

--- seg4diff/modeling/__init__.py
+++
@@ -1,4 +0,0 @@
-# Copyright (c) Facebook, Inc. and its affiliates.
-from .backbone.sd3_backbone import SD3Backbone
-from .backbone.sd35_backbone import SD35Backbone
-from .backbone.flux_backbone import FluxBackbone
